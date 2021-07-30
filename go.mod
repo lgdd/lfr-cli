c65d899a08db4c3fd6e48754bed542a56af710a1
lgdd <louis-guillaume.durand@liferay.com>
2021-07-30 16:59:04 +0200

    feat: add interactive mode for create command

--- go.mod
+++ go.mod
@@ -5,7 +5,12 @@ go 1.16
 require (
 	github.com/fatih/color v1.9.0
 	github.com/iancoleman/strcase v0.1.3
+	github.com/juju/ansiterm v0.0.0-20210706145210-9283cdf370b5 // indirect
+	github.com/lunixbochs/vtclean v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5
+	github.com/manifoldco/promptui v0.8.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/mattn/go-runewidth v0.0.12 // indirect
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/nxadm/tail v1.4.8
@@ -15,6 +20,6 @@ require (
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20210503080704-8803ae5d1324 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
 )
--- lfr/pkg/cmd/create/create.go
+++ lfr/pkg/cmd/create/create.go
@@ -1,15 +1,26 @@
 package create
 
 import (
-	"github.com/spf13/cobra"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
 
+	"github.com/iancoleman/strcase"
 	"github.com/lgdd/liferay-cli/lfr/pkg/project"
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
 )
 
 var (
 	Cmd = &cobra.Command{
 		Use:   "create TYPE NAME",
 		Short: "Create a Liferay project",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				promptCreateChoices(cmd, args)
+			}
+		},
 	}
 )
 
@@ -21,4 +32,115 @@ func init() {
 	Cmd.AddCommand(createServiceBuilder)
 	Cmd.AddCommand(createDocker)
 	Cmd.PersistentFlags().StringVarP(&project.PackageName, "package", "p", "org.acme", "base package name")
+}
+
+func promptCreateChoices(cmd *cobra.Command, args []string) {
+	promptTemplate := promptui.Select{
+		Label: "Choose a template",
+		Items: []string{"api", "docker", "mvc-portlet", "service-builder", "spring-mvc-portlet", "workspace"},
+	}
+
+	_, template, err := promptTemplate.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	if template == "docker" {
+		promptJavaVersion := promptui.Select{
+			Label: "Choose a Java version",
+			Items: []string{"8", "11"},
+		}
+		_, javaVersion, err := promptJavaVersion.Run()
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			os.Exit(1)
+		}
+
+		promptBuildOption := promptui.Select{
+			Label: "Choose a build option",
+			Items: []string{"Multi-stage", "Single stage"},
+		}
+
+		_, buildOption, err := promptBuildOption.Run()
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			os.Exit(1)
+		}
+
+		if buildOption == "Multi-stage" {
+			os.Args = append(os.Args, template, "-j", javaVersion, "-m")
+		} else {
+			os.Args = append(os.Args, template, "-j", javaVersion)
+		}
+
+		cmd.Execute()
+		return
+	}
+
+	promptName := promptui.Prompt{
+		Label: "Choose a name",
+		Validate: func(input string) error {
+			if len(input) == 0 {
+				return errors.New("the name cannot be empty")
+			}
+			return nil
+		},
+	}
+
+	name, err := promptName.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	workspacePackage, _ := project.GetGroupId()
+	defaultPackageName := strings.Join([]string{workspacePackage, strcase.ToDelimited(name, '.')}, ".")
+
+	if template == "workspace" {
+		defaultPackageName = "org.acme"
+	}
+
+	promptPackageName := promptui.Prompt{
+		Label:   "Choose a package name",
+		Default: defaultPackageName,
+		Validate: func(input string) error {
+			if len(input) == 0 {
+				return errors.New("the name cannot be empty")
+			}
+			return nil
+		},
+	}
+
+	packageName, err := promptPackageName.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	if template == "spring-mvc-portlet" {
+		promptTemplateEngine := promptui.Select{
+			Label: "Choose a template engine",
+			Items: []string{"thymeleaf", "jsp"},
+		}
+
+		_, templateEngine, err := promptTemplateEngine.Run()
+
+		if err != nil {
+			fmt.Printf("Prompt failed %v\n", err)
+			os.Exit(1)
+		}
+
+		os.Args = append(os.Args, template, name, "-p", packageName, "-t", templateEngine)
+		cmd.Execute()
+		return
+	}
+
+	os.Args = append(os.Args, template, name, "-p", packageName)
+	cmd.Execute()
 }
