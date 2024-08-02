43c5a7e4f487700532d1222b869eefd000fa3f0d
lgdd <louis-guillaume.durand@liferay.com>
2024-08-02 07:39:51 +0200

    feat: add diagnose info on workspace creation

--- internal/cmd/create/create_workspace.go
+++ internal/cmd/create/create_workspace.go
@@ -3,6 +3,7 @@ package create
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/lgdd/lfr-cli/internal/cmd/exec"
 	"github.com/lgdd/lfr-cli/internal/conf"
@@ -10,6 +11,7 @@ import (
 	"github.com/lgdd/lfr-cli/pkg/scaffold"
 	"github.com/lgdd/lfr-cli/pkg/util/fileutil"
 	"github.com/lgdd/lfr-cli/pkg/util/logger"
+	"github.com/lgdd/lfr-cli/pkg/util/procutil"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -82,4 +84,29 @@ func runInit(name, build string) {
 func printInitCmd(name string) {
 	logger.Println("\nInitialize your Liferay bundle:")
 	logger.PrintlnInfo(fmt.Sprintf("cd %s && lfr init\n", name))
+	major, version, err := procutil.GetCurrentJavaVersion()
+
+	if err != nil {
+		logger.PrintError("[✗] ")
+		logger.Print("Liferay requires Java 8, 11, 17 or 21.\n")
+	}
+
+	if major == "8" {
+		logger.PrintWarn("[!] ")
+		logger.Printf("Java %s is used\n", strings.Split(version, "\n")[0])
+		logger.PrintWarn("Liferay DXP 2024.Q1 and Liferay Portal 7.4 GA112 will be the last version to support Java 8.")
+	} else if major == "11" {
+		logger.PrintWarn("[!] ")
+		logger.Printf("Java %s is used\n", strings.Split(version, "\n")[0])
+		logger.PrintWarn("Liferay DXP DXP 2024.Q2 and Liferay Portal 7.4 GA120 will be the last version to support Java 11.")
+	} else if major == "17" || major == "21" {
+		logger.PrintSuccess("[✓] ")
+		logger.Printf("Java %s is used\n", strings.Split(version, "\n")[0])
+		logger.PrintlnBold("Liferay DXP 2024.Q2+ and Liferay Portal CE 7.4 GA120+ are fully certified to run on both Java JDK 17 and 21.")
+		logger.PrintfBold("Building with Java %s requires Gradle 8.5 and Liferay Gradle Workspace Plugin v10.1+.", major)
+	} else {
+		logger.PrintWarn("[!] ")
+		logger.Printf("Java %s is used\n", strings.Split(version, "\n")[0])
+		logger.PrintlnBold("Liferay only supports Java 8, 11, 17 and 21. See https://www.liferay.com/compatibility-matrix.")
+	}
 }
