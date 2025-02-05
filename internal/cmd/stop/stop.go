869a483d1a2aec4974abe5469fce8a3fa62a19a6
lgdd <louis-guillaume.durand@liferay.com>
2025-02-05 09:35:10 +0100

    feat: improved stop command feedback

--- internal/cmd/stop/stop.go
+++ internal/cmd/stop/stop.go
@@ -1,15 +1,20 @@
 package stop
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
 
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/lgdd/lfr-cli/internal/conf"
 	"github.com/lgdd/lfr-cli/pkg/util/fileutil"
 	"github.com/lgdd/lfr-cli/pkg/util/logger"
 
 	"github.com/lgdd/lfr-cli/pkg/util/procutil"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -48,4 +53,52 @@ func run(cmd *cobra.Command, args []string) {
 		logger.Fatal(err.Error())
 	}
 
+	isRunning, pid, err := procutil.IsCatalinaRunning()
+
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	if !isRunning {
+		logger.Print("Liferay is already stopped.")
+		os.Exit(0)
+	}
+
+	_ = spinner.New().
+		Title(fmt.Sprintf("Stopping Liferay (pid=%v)", pid)).
+		Action(checkLiferayStatus).
+		Accessible(viper.GetBool(conf.OutputAccessible)).
+		Run()
+
+	isRunning, pid, err = procutil.IsCatalinaRunning()
+
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	if isRunning {
+		logger.PrintlnWarn("Liferay is still running.")
+		logger.PrintfWarn("Be patient, but you might need to kill the process (pid=%v).", pid)
+	} else {
+		logger.PrintSuccess("Liferay stopped successfully.")
+	}
+}
+
+func checkLiferayStatus() {
+	isRunning, _, err := procutil.IsCatalinaRunning()
+
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	maxIterations := 60
+	iteration := 0
+	for isRunning && iteration < maxIterations {
+		iteration = iteration + 1
+		isRunning, _, err = procutil.IsCatalinaRunning()
+		if err != nil {
+			logger.Fatal(err.Error())
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
